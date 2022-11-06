97af03b9c30f6484f7c85c83cbdf49fdebab6ce0
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-11-06 13:59:49 +0100

    PRODUCT and there id route was configured.

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const replaceTemplates = (temp, product) => {
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTIRENTS%}/g, product.nutrients);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
@@ -66,7 +66,7 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    // console.log(req.url);
     // [true] -> query string.
     // console.log(url.parse(req.url, true));
 
@@ -87,8 +87,13 @@ const server = http.createServer((req, res) => {
         // res.end(tempOverview);
 
     // * Product page
-    } else if(pathname === '/product') {
-        res.end('<h1>See The Product</h1>')
+    } else if (pathname === '/product') {
+        // console.log(query);
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplates(tempProduct, product)
+        // res.end('<h1>See The Product</h1>')
+        res.end(output)
 
     // * API
     } else if (pathname == '/api') {

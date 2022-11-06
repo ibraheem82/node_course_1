3d1af49e7dbca4ff0175110fb387decd1c614ba5
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-11-06 21:51:06 +0100

    replace template was removed.

--- index.js
+++ index.js
@@ -41,20 +41,7 @@ const PORT = 8000;
 // console.log('Will read file.');
 
 // ####################### Working with Server ############################
-const replaceTemplates = (temp, product) => {
-    // * productName from the json file.
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    // * if it false.
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
+
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
